949aeafcd4ed6d06e7b94cc0537de31de3e5834e
omar3o3 <otantawy53@gmail.com>
2022-07-11 17:53:12 -0400

    commit tree ec5ad5a0decdd1f806df5ead9d0cbf1460f63c7e

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,6 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :check_if_member
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -11,10 +12,18 @@ class MembersOnlyArticlesController < ApplicationController
     render json: article
   end
 
+
+
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+
+  def check_if_member
+    unless session[:user_id]
+      render json: { error: "Not authorized"}, status: 401
+    end
   end
 
 end
